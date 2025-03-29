a4b5d9f3ed1f7829f01a626ee22128319244efac
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-29 00:31:41 -0700

    turn in for the night
    lost a certain amount of time getting mad about python again...

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ This program requires Python 3.12 or later, but purely in order to use the Type 
 """
 
 from sys import argv
+# from collections.abc import Callable # do I need this?
 
-def generalized_state_search_brute_forcer[S1, S2](initial_state: S1, desired_state: S2, rules: Callable[[S1], S2], print_debug_information: bool = False) -> []:
+def generalized_state_search_brute_forcer[S](initial_state: S, desired_state: S, production_rules: Callable[[S], S], print_debug_information: bool = False) -> list[S]:
   """This function searches state space starting from initial_state until it finds desired_state. May take a while.
-  Note that the types S1 and S2 (meant to stand for State, and representing whatever type you bring to the table for that, like a dict or whatever) almost certainly have to be the same. If they're different, you can only have a chain of states 1 link long! But I've left in that edge case out of perversity."""
-
+  Return value is a list of states from initial to desired. No information about which rules were used is returned. (COULD: extend the program to do so.)
+  The type S is meant to stand for State, and represents whatever type you bring to the table for that, like a dict or whatever."""
+  all_states: set = set(initial_state) # set insertion order is conserved in python (I think?) so this set can also function as a queue (I think?)
+  reversewalkable_links = dict()
+  while True:
+    for s in all_states:
+      for r in production_rules:
+        result = r(s)
+        if result == desired_state:
+          # we win...
+        else:
+          reversewalkable_links.add(result, s)
+          all_states.insert()
+  return array_of_states
 if __name__ == "__main__":
   print(f"{argv[0]} has no main funciton, because it is a library and not a script. Try importing it and then using your ")
